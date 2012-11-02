a92c2aa7e85d83e75561b8f82d448a838a98ec1a
Sam Wilson <tecywiz121@hotmail.com>
2012-11-02 10:24:40 -0400

    Change media/static settings; Add a copy of python's bisect.py module for pyjs

--- webpong/settings.py
+++ webpong/settings.py
@@ -12,6 +12,10 @@ MANAGERS = ADMINS
 import dj_database_url
 DATABASES = {'default': dj_database_url.config()}
 
+from os.path import join as p_join, dirname, realpath
+
+BASE_DIR = realpath(p_join( dirname( __file__ ), '..' ))
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -37,18 +41,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = p_join(BASE_DIR, 'mediafiles')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = p_join(BASE_DIR, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -59,6 +63,9 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    p_join(BASE_DIR, 'static'),         # Project-wide static files
+    p_join(BASE_DIR, 'sim', 'output'),  # pyjs compiled output
 )
 
 # List of finder classes that know how to find static files in
