abb8635a2d77a71fb58e45b28570d6e4d2d5150e
Sam Wilson <tecywiz121@hotmail.com>
2012-11-02 10:27:25 -0400

    Add an empty templates folder

--- webpong/settings.py
+++ webpong/settings.py
@@ -102,6 +102,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    p_join(BASE_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
