c70d6102f580bb8e169261390457c0291f3973ab
Sam Wilson <tecywiz121@hotmail.com>
2012-11-04 20:29:36 -0500

    Add a localsettings file

---
+++ webpong/local_settings.py
@@ -0,0 +1,16 @@
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '6$q0_n4fu42!on1#&amp;phf85ay=f_9ry^^esiiq8*_xd_=37p_%@'
+
+EMAIL_HOST_PASSWORD = 'ddb6b931-b2f9-4d1c-b905-abe329f1d961'
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'development.db',                      # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
